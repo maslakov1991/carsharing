eff2c585e4eec1da5cc581fa3156c5456ea95d64
Maslakov Olskeii <a.maslakov1991@gmail.com>
2023-02-13 16:34:16 +0200

    pre-adaptive

--- js/main.js
+++ js/main.js
@@ -5,4 +5,11 @@ $(function () {
         autoplay: true,
         fade: true
     })
+
+    $(".reviews-slider").slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 4,
+        slidesToScroll: 1
+    })
 });
