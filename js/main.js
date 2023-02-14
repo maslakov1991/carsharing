d0e15a55bc3d2a8586ad4755b2fea56675483a51
Maslakov Olskeii <a.maslakov1991@gmail.com>
2023-02-14 11:54:47 +0200

    adaptive done

--- js/main.js
+++ js/main.js
@@ -3,14 +3,42 @@ $(function () {
         arrows: false,
         dots: true,
         autoplay: true,
-        fade: true
+        fade: true,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    dots: false
+                }
+            },
+        ]
     })
 
     $(".reviews-slider").slick({
         arrows: false,
         dots: true,
         slidesToShow: 4,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1101,
+                settings: {
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 861,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 530,
+                settings: {
+                    slidesToShow: 1
+                }
+            },
+        ]
     })
 
     $(".menu__btn").on('click', function () {
